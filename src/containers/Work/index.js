cedcf0966acfa7fa2ae4cc236511c5a077b1f1d4
hazratgs <hazratgs@gmail.com>
2018-07-08 15:01:08 +0300

    fix

--- src/containers/Work/index.js
+++ src/containers/Work/index.js
@@ -1,9 +1,14 @@
 import React, { PureComponent } from 'react'
+import { withRouter } from 'react-router-dom'
+import CSSModules from 'react-css-modules'
+import s from './style.pcss'
 
+@withRouter
+@CSSModules(s)
 export default class Work extends PureComponent {
   render () {
     return (
-      <div>В процессе...</div>
+      <div styleName='work'>В процессе...</div>
     )
   }
 }
