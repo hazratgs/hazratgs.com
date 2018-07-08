340386259f149367e69a992dd476527f90b6a725
hazratgs <hazratgs@gmail.com>
2018-07-08 14:18:51 +0300

    add component footer

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -5,8 +5,21 @@ import s from './style.pcss'
 @CSSModules(s)
 export default class Header extends PureComponent {
   render () {
+    const year = new Date().getFullYear()
     return (
-      <div>шапка</div>
+      <footer styleName='footer'>
+        <div styleName='top'>
+          <a href='tel:+79282194199'>+7 (928) 219-41-99</a>
+          <a href='mailto:hazratgs@gmail.com'>hazratgs@gmail.com</a>
+          <a target='_blank' href='https://t.me/hazratgs'>Telegram</a>
+          <a target='_blank' href='https://github.com/hazratgs'>GitHub</a>
+          <a target='_blank' href='https://habr.com/users/hazratgs/'>Хабрахабр</a>
+          <a target='_blank' href='https://moikrug.ru/hazratgs'>Мой круг</a>
+        </div>
+        <div styleName='bottom'>
+          © 2012–{year} Хазрат Гаджикеримов
+        </div>
+      </footer>
     )
   }
 }
